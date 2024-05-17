1a92522399d742406b2e7d3dafb3a92e9b18326a
Bielz77 <165225445+Bielz77@users.noreply.github.com>
2024-05-17 06:04:47 -0300

    Add files via upload

---
+++ area.py
@@ -0,0 +1,7 @@
+
+largura = float(input('Entre com a largura: '))
+altura = float(input('Entre com a altura: '))
+area = largura * altura
+perimetro = 2 * (altura + altura)
+print(f'A área do retângulo é: {area}')
+print(f'O perimetro do retângulo é: {perimetro}')
